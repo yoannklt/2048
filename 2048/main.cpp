bd0a1579cfbc0d93958b324a791bc28975a3c933
Kerly <115028766+yoannklt@users.noreply.github.com>
2023-10-23 12:34:10 +0200

    Create main.cpp

---
+++ 2048/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void displayGrid(int tableau[4][4]);
+
+int main(int argc, char* argv[])
+{
+	int tableau[4][4] = { 0 };
+
+	displayGrid(tableau);
+
+	return 0;
+}
+
+void displayGrid(int tableau[4][4])
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			std::cout << tableau[i][j];
+		}
+		std::cout << "\n";
+	}
+
+}
